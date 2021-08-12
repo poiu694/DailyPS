aac3076ec7b8cfdd1602cf8741fd53f5ab586146
Lee <43488305+poiu694@users.noreply.github.com>
2021-08-12 13:51:10 +0900

    Programmers Weekly 2, map 🤩

---
+++ WeeklyProgrammers/2주차_1.js
@@ -0,0 +1,7 @@
+function solution(scores) {
+    return scores.map((_, c) => scores.map(r => r[c]))
+            .map((s, i) => [...s.splice(i, 1), s])
+           .map(([m, s]) => Math.min(...s) <= m && m <= Math.max(...s) ? [m, ...s] : s)
+           .map(s => "FDDCBAA"[Math.max(parseInt(s.reduce((a, c) => a + c) / s.length / 10) - 4, 0)])
+            .join("");
+}
