891bcd11b12fcb0b6b50873b4294e3b2edd2c498
Lee <43488305+poiu694@users.noreply.github.com>
2021-11-11 16:43:38 +0900

    BOJ 1744 Greedy multiply or plus

--- Greedy/1744.cpp
+++ Greedy/1744.cpp
@@ -3,34 +3,73 @@
 
 using namespace std;
 
-int N;
-priority_queue<int, vector<int>, less<int>> maxQ;
-
-void init(){
+void fast_io(void)
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 }
 
-void input(){
-    cin >> N;
+int	get_max_sum_of_pair_number_from_queue(priority_queue<int> &pq, int sign, int zeroCnt)
+{
+	int	a, b;
+	int	sum;
 
-    int temp;
-    for(int i=0; i<N; i++){
-        cin >> temp;
-        maxQ.push(temp);
-    }
+	sum = 0;
+	while (!pq.empty())
+	{
+		if (pq.size() == 1)
+			break ;
+		if (pq.top() == 1 and sign == 1)
+			break ;
+		a = pq.top();
+		pq.pop();
+		if (pq.top() == 1 and sign == 1)
+		{
+			pq.push(a);
+			break ;
+		}
+		b = pq.top();
+		pq.pop();
+		sum += a * b;
+	}
+	while (!pq.empty())
+	{
+		if (sign == -1 and zeroCnt)
+			break ;
+		sum += pq.top() * sign;
+		pq.pop();
+	}
+	return (sum);
 }
 
-void solve(){
-    while(!maxQ.empty()){
-        cout << maxQ.top() << " ";
-        maxQ.pop();
-    }
+void get_solution(void)
+{
+	int					N, temp;
+	int					ans;
+	int					zeroCnt;
+	priority_queue<int>	minusQ;
+	priority_queue<int>	plusQ;	
+
+	cin >> N;
+	zeroCnt = 0;
+	while (N--)
+	{
+		cin >> temp;
+		if (temp > 0)
+			plusQ.push(temp);
+		else if (temp < 0)
+			minusQ.push(-temp);
+		else
+			zeroCnt++;
+	}
+	ans = get_max_sum_of_pair_number_from_queue(plusQ, 1, zeroCnt);
+	ans += get_max_sum_of_pair_number_from_queue(minusQ, -1, zeroCnt);
+	cout << ans;
 }
 
-int main(){
-    init();
-    input();
-    solve();
+int main(void)
+{
+    fast_io();
+	get_solution();
 }
