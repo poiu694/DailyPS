8af2257f9222e1baac63f7d5601bf0df645e31a9
Lee <43488305+poiu694@users.noreply.github.com>
2021-12-04 01:03:06 +0900

    add algorithm form with c++

---
+++ UOSPC/form.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+void    fast_io(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void    get_solution(void)
+{
+    int N, M;
+
+    cin >> N >> M;
+    cout << N + M;
+}
+
+int main(void)
+{
+    fast_io();
+    get_solution();
+}
