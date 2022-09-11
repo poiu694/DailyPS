90c12ffcb3038e8553ee444f35e346488f63457c
Lee <43488305+poiu694@users.noreply.github.com>
2022-09-11 12:14:33 +0900

    feat: 요구 충족시키는 최소 시간찾기 dijkstra

--- 2022KAKAO_INTERNSHIP/코딩테스트공부.cpp
+++ 2022KAKAO_INTERNSHIP/코딩테스트공부.cpp
@@ -1,3 +1,4 @@
+// Version 1. DP
 #include <cstring>
 #include <vector>
 #include <cmath>
@@ -43,4 +44,89 @@ int solution(int alp, int cop, vector<vector<int>> problems) {
         }
     }
     return (dp[maxAlp][maxCop]);
+}
+
+// Version 2. Dijkstra
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <climits>
+
+using namespace std;
+using pii = pair<int, int>;
+
+struct Info {
+	int alp;
+	int cop;
+    int time;
+};
+
+struct compare {
+	bool operator()(const Info& info1, const Info& info2) {
+        if (info1.time == info2.time) {
+            if (info1.alp == info2.alp) {
+                return (info1.cop < info2.cop);
+            }
+            return (info1.alp < info2.alp);
+        }
+		return info1.time > info2.time;
+	}
+};
+
+bool    isCanSolveProblem(pii cur, pii req) {
+    return (cur.first >= req.first && cur.second >= req.second);
+}
+
+vector<vector<int>>    dijkstra(int alp, int cop, int maxAlp, int maxCop, vector<vector<int>> problems) {
+    vector<vector<int>> dist(maxAlp * 5, vector<int>(maxCop * 5, INT_MAX));
+    priority_queue<Info, vector<Info>, compare> pq;
+    
+    pq.push({ alp, cop, 0 });
+    dist[alp][cop] = 0;
+    while (!pq.empty()) {
+        Info current = pq.top();
+        pq.pop();
+        
+        if (isCanSolveProblem({ current.alp, current.cop }, { maxAlp, maxCop })) {
+            break ;
+        }
+        if (current.alp < maxAlp && dist[current.alp][current.cop] + 1 < dist[current.alp + 1][current.cop]) {
+            dist[current.alp + 1][current.cop] = dist[current.alp][current.cop] + 1;
+            pq.push({ current.alp + 1, current.cop, dist[current.alp][current.cop] + 1 });
+        }
+        if (current.cop < maxCop && dist[current.alp][current.cop] + 1 < dist[current.alp][current.cop + 1]) {
+            dist[current.alp][current.cop + 1] = dist[current.alp][current.cop] + 1;
+            pq.push({ current.alp, current.cop + 1, dist[current.alp][current.cop] + 1 });
+        }
+        for (vector<int> problem: problems) {
+            if (isCanSolveProblem({ current.alp, current.cop }, { problem[0], problem[1] })) {
+                int nextI = current.alp + problem[2] > maxAlp ? maxAlp : current.alp + problem[2];
+                int nextJ = current.cop + problem[3] > maxCop ? maxCop : current.cop + problem[3];
+                if (dist[nextI][nextJ] > dist[current.alp][current.cop] + problem[4]) {
+                    dist[nextI][nextJ] = dist[current.alp][current.cop] + problem[4];
+                    pq.push({
+                        nextI, nextJ, dist[current.alp][current.cop] + problem[4]
+                    });
+                }
+                
+            }
+        }
+    }
+    return (dist);
+}
+
+int solution(int alp, int cop, vector<vector<int>> problems) {
+    int maxAlp;
+    int maxCop;
+    vector<vector<int>> dist;
+    
+    maxAlp = alp;
+    maxCop = cop;
+    for (vector<int> problem: problems) {
+        maxAlp = max(maxAlp, problem[0]);
+        maxCop = max(maxCop, problem[1]);
+    }
+    dist = dijkstra(alp, cop, maxAlp, maxCop, problems);
+    return (dist[maxAlp][maxCop]);
 }
