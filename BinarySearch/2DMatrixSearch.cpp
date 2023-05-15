1a525aca048bf679e721738ddc7b3358b8faf5f5
Lee <43488305+poiu694@users.noreply.github.com>
2023-05-15 15:53:05 +0900

    feat: leetcode 2dMAtrixSearch

---
+++ BinarySearch/2DMatrixSearch.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    private:
+        vector<int> list;
+    public:
+
+    bool    binarySearch(int target)
+    {
+        int left = 0;
+        int right = this->list.size() - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (this->list[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else if (this->list[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                return (true);
+            }
+        }
+        return (false);
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        this->list.resize(matrix.size() * matrix[0].size());
+
+        int index = 0;
+        for (auto m: matrix)
+            for (int a: m)
+                this->list[index++] = a;
+        return (this->binarySearch(target));
+    }
+};
