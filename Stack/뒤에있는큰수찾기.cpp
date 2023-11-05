7455b640aac4027c556efcee057f740f1b5f2758
Lee <43488305+poiu694@users.noreply.github.com>
2023-11-05 15:49:00 +0900

    Programmers 뒤에있는 큰 수 찾기

---
+++ Stack/뒤에있는큰수찾기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer(numbers.size(), -1);
+    stack<int>  st;
+    
+    for (int i=0; i<numbers.size(); i++)
+    {
+        while (!st.empty() && numbers[i] > numbers[st.top()])
+        {
+            answer[st.top()] = numbers[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return answer;
+}
